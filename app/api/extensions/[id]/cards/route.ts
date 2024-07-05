f42078fd059dbf25714a4a990eba1e3591d83552
rosesarered <paul.souille85@gmail.com>
2024-07-06 01:04:02 +0200

    feat(cards): make load more work

--- app/api/extensions/[id]/cards/route.ts
+++ app/api/extensions/[id]/cards/route.ts
@@ -1,14 +1,16 @@
 import { PrismaClient } from '@prisma/client';
+import { randomInt } from 'crypto';
 import { NextApiRequest } from 'next';
 import { NextResponse } from "next/server";
 const prisma = new PrismaClient()
 
 
-export async function GET(request: NextApiRequest& {nextUrl: {searchParams:URLSearchParams}},  {params}:{params:{id:string}} ,) {
+export async function GET(request: NextApiRequest & {nextUrl: {searchParams:URLSearchParams}},  {params}:{params:{id:string}} ,) {
     console.log(`Fetching cards for id ${params.id}`)
     const id = params.id;
     const page = parseInt(request.nextUrl.searchParams.get('page') || '1');
     const pageSize = parseInt(request.nextUrl.searchParams.get('pageSize') || '20');
+    const query = request.nextUrl.searchParams.get('query') || '';
   
     if (!id) {
       return NextResponse.json({ error: 'ExtensionId is required' }, { status: 400 });
@@ -18,13 +20,17 @@ export async function GET(request: NextApiRequest& {nextUrl: {searchParams:URLSe
       const skip = (page - 1) * pageSize;
       let cards = await prisma.cards.findMany({
         where: {
-          extensionId: parseInt(id)
+          extensionId: parseInt(id),
+          // body: {
+          //   search: query,
+          // },
         },
         skip: skip,
         take: pageSize,
         orderBy: {
           printedName: 'asc'
-        }
+        },
+        
       });
   
       const totalCards = await prisma.cards.count({
@@ -33,6 +39,7 @@ export async function GET(request: NextApiRequest& {nextUrl: {searchParams:URLSe
         }
       });
       cards = cards.map((card)=>{
+        card.stock = randomInt(3);
         return card
       })
       return NextResponse.json({
--- app/cards/[id]/page.tsx
+++ app/cards/[id]/page.tsx
@@ -3,66 +3,71 @@ import SearchBar from "@/app/components/searchbar";
 import useCards from "@/app/hooks/useCards";
 import { Image } from "@nextui-org/image";
 import { Button } from "@nextui-org/react";
-import Link from "next/link";
+import { Cards } from "@prisma/client";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
 
 const CardDetail = () => {
   const { id }: { id: string } = useParams();
-  const page = 1;
+  const initialPage = 1;
   const pageSize = 20;
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [page, setPage] = useState(initialPage);
+  const [cards, setCards] = useState<Cards[]>([]);
+  const { data, loading } = useCards(id, page, pageSize);
 
-  let { data, loading } = useCards(id, page, pageSize);
+  useEffect(() => {
+    if (data && data.cards) {
+      setCards((prevCards) => {
+        setLoadingMore(false);
+        return [...prevCards, ...data.cards];
+      });
+    }
+  }, [data]);
 
-  if (loading) {
+  const loadMoreCards = () => {
+    setLoadingMore(true);
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  if (loading && page === initialPage) {
     return <p>Loading...</p>;
   }
-
   return (
     <div>
       <SearchBar />
       <div className=" grid gap-16 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-        {data!.cards.map((card) => (
+        {cards.map((card) => (
           <div key={card.id}>
-            <Link href={`/cards/${card.id}`}>
-              <Image
-                isZoomed
-                width={300}
-                alt={`${card.name}'s image`}
-                src={`data:image/png;base64,${card.smallImage}`}
-              />
-            </Link>
+            <Image
+              className={card.stock > 0 ? "" : styles.notInStock}
+              isZoomed
+              width={300}
+              alt={`${card.name}'s image`}
+              src={`data:image/png;base64,${card.smallImage}`}
+            />
           </div>
         ))}
       </div>
       <div className="flex content-center	justify-center	mb-5 mt-5">
-        <Button>Load more</Button>
+        <div className="flex-grow"></div>
+        <div>
+          <Button
+            onPress={loadMoreCards}
+            isLoading={loadingMore}
+            disabled={loading}
+          >
+            {loadingMore ? "Loading..." : "Load more"}
+          </Button>
+        </div>
+        <div className="flex-grow"></div>
+        <div className="flex justify-center content-center flex-wrap font-semibold">
+          {cards.length} / {data?.totalCards}
+        </div>
       </div>
     </div>
   );
 };
 
 export default CardDetail;
-
-// const initialPage = 1;
-// const pageSize = 20;
-
-// const [page, setPage] = useState(initialPage);
-// const [cards, setCards] = useState([]);
-// const { data, loading } = useCards(id, page, pageSize);
-
-// useEffect(() => {
-//   if (data && data.cards) {
-
-//     setCards((prevCards) => [...prevCards, ...data.cards]);
-//   }
-// }, [data]);
-
-// const loadMoreCards = () => {
-//   setPage((prevPage) => prevPage + 1);
-// };
-
-// if (loading && page === initialPage) {
-//   return <p>Loading...</p>;
-// }        <Button onPress={loadMoreCards} disabled={loading}>
-// {loading ? "Loading..." : "Load more"}
-// </Button>
