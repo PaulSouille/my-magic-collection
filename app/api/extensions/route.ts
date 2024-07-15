34eb141b7cee18b18c0a0d2a72d1452da221e1af
rosesarered <paul.souille85@gmail.com>
2024-07-15 18:33:11 +0200

    debug

--- app/api/extensions/route.ts
+++ app/api/extensions/route.ts
@@ -3,8 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
-  const extensions = await prisma.extensions.findMany();
+  const extensions = await prisma.cards.findMany();
   console.log(prisma.extensions)
+  return NextResponse.json(extensions)
   if (extensions == null || extensions == undefined) {
     return NextResponse.json({ error: "Error occured", status: 400 });
   }
