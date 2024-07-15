976a76985e574eb1a96d3aa5a401d6fc53fb7e6d
rosesarered <paul.souille85@gmail.com>
2024-07-15 17:44:35 +0200

    fix(type): use right request type

--- app/api/extensions/route.ts
+++ app/api/extensions/route.ts
@@ -1,20 +1,12 @@
-import { PrismaClient } from '@prisma/client';
-import { NextResponse } from "next/server";
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+const prisma = new PrismaClient();
 
-export async function GET(req: Request) {
-  try{
-    const extensions = await prisma.extensions.findMany();
-  
-      if(extensions == null || extensions == undefined){
-          return NextResponse.json({error:"Error occured", status:400});
-      }
-      return NextResponse.json(extensions);
+export async function GET(req: NextRequest) {
+  const extensions = await prisma.extensions.findMany();
 
-  } catch(error){
-    console.log(error)
+  if (extensions == null || extensions == undefined) {
+    return NextResponse.json({ error: "Error occured", status: 400 });
   }
-  
-  
+  return NextResponse.json(extensions);
 }
-
