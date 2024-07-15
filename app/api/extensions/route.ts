415056201ffc2e06e50abae457dc1ab62830ea17
rosesarered <paul.souille85@gmail.com>
2024-07-15 18:57:06 +0200

    debug

--- app/api/extensions/route.ts
+++ app/api/extensions/route.ts
@@ -20,3 +20,21 @@ export async function GET(req: NextRequest) {
     await prisma.$disconnect();
   }
 }
+
+export async function POST(req: NextRequest) {
+  try {
+    const extensions = await prisma.extensions.findMany();
+    
+    if (!extensions) {
+      console.error("No extensions found");
+      return NextResponse.json({ error: "Error occurred", status: 400 });
+    }
+
+    return NextResponse.json(extensions);
+  } catch (error) {
+    console.error("Database query failed", error);
+    return NextResponse.json({ error: "Error occurred", status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
